844926db2515b10d52015ab8d4992b3bae9f10ef
neths21 <83169524+neths21@users.noreply.github.com>
2025-07-18 00:00:52 +0530

    frontend  change

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL
+    baseURL: "https://collaborative-todo-board-2.onrender.com/"
 });
 
 export default api;
