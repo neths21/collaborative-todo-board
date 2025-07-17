3fc9e98f288a0cfc8172d53c2f93f9c42538fae9
neths21 <83169524+neths21@users.noreply.github.com>
2025-07-18 00:24:00 +0530

    final deployment

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: "https://collaborative-todo-board-2.onrender.com/"
+    baseURL: "https://collaborative-todo-board-2.onrender.com/api/"
 });
 
 export default api;
